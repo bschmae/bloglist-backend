9c03fd2772dff7c6fbc1c151873f775829a90d94
William Schmae <williamschmae@Williams-MacBook-Pro.local>
2023-02-27 15:38:13 -0700

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,38 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  };
+  return null
+};
   
 blogRouter.post('/', async (request, response) => {
     const body = request.body;
-    const user = await User.findById(body.userId);
 
-    if (!(request.body.likes)) {
-      request.body.likes = 0;
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' });
+    };
+
+    const user = await User.findById(decodedToken.id);
+
+    if (!(body.likes)) {
+      body.likes = 0;
     };
 
     const blog = new Blog({
-      ...request.body,
+      ...body,
       user: user,
     });
 
