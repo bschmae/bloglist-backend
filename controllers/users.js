5b5bed946794263a015833881d1d394520d7a28d
William Schmae <williamschmae@Williams-MacBook-Pro.local>
2023-02-27 13:29:20 -0700

    4.17

--- controllers/users.js
+++ controllers/users.js
@@ -25,7 +25,7 @@ userRouter.post('/', async (request, response) => {
 });
 
 userRouter.get('/', async (request, response) => {
-    const user = await User.find({});
+    const user = await User.find({}).populate('blogs', { title: 1, url: 1});
     response.json(user);
 })
 
