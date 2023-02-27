60dee831fb65b6108aeae7bdb099c63f50c36f70
William Schmae <williamschmae@Williams-MacBook-Pro.local>
2023-02-27 14:15:14 -0700

    4.18

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config');
 const logger = require('./utils/logger');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
